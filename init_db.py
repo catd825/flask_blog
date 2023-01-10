6767aff39c2a6a704f146ca44c2e408d5ac52b7c
catd825 <60204063+catd825@users.noreply.github.com>
2023-01-10 15:21:59 -0500

    seeded comments

--- init_db.py
+++ init_db.py
@@ -15,5 +15,21 @@ cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
             ('Second Post', 'Content for the second post')
             )
 
+cur.execute("INSERT INTO comments (post_id, content) VALUES (?, ?)",
+            (1, 'text for second comment')
+            )
+
+cur.execute("INSERT INTO comments (post_id, content) VALUES (?, ?)",
+            (1, 'text for second comment')
+            )
+
+cur.execute("INSERT INTO comments (post_id, content) VALUES (?, ?)",
+            (2, 'text for third comment')
+            )
+
+cur.execute("INSERT INTO comments (post_id, content) VALUES (?, ?)",
+            (1, 'fawefawefewfawef')
+            )
+
 connection.commit()
 connection.close()
